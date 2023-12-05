5e8fefe8b081475e14a038bb535cbf033134a298
Ashish Neupane <ashishneupane1997@gmail.com>
2023-12-05 10:05:43 -0500

    fix: change specific property name

--- app/ontario/page.js
+++ app/ontario/page.js
@@ -1,73 +1,72 @@
-import ResoCard from '@/components/reso/ResoCard';
-import Link from 'next/link';
-import React from 'react';
-import SearchBar from '@/components/reso/SearchBar';
+import ResoCard from "@/components/reso/ResoCard";
+import Link from "next/link";
+import React from "react";
+import SearchBar from "@/components/reso/SearchBar";
 
 const page = async () => {
-  const city = "ontario";
+  const city = "Ontario";
 
-
-  const url =`http://localhost:3000/residential/Properties`;
-const options = {
-  method: "GET",
-};
-const res = await fetch(url, options);
-const data = await res.json();
-const main_data = data;
-
-
+  const url = `localhost:3000/residential/Properties/?$limit=10&$skip=0&$select=Province=${city}`;
+  const options = {
+    method: "GET",
+  };
+  const res = await fetch(url, options);
+  const data = await res.json();
+  const main_data = data.results();
 
   return (
     <>
-    <div className="container-fluid">
-    <div className="input-group input-group-search me-2 me-md-0">
+      <div className="container-fluid">
+        <div className="input-group input-group-search me-2 me-md-0">
           <SearchBar />
-          
-            <button
-              className="input-group-text btn bg-light2 bg-lh mybtn d-block py-search"
-              type="button"
-              aria-label="Search Button"
+
+          <button
+            className="input-group-text btn bg-light2 bg-lh mybtn d-block py-search"
+            type="button"
+            aria-label="Search Button"
+          >
+            <svg
+              aria-hidden="true"
+              className="svg"
+              viewBox="0 0 30 30"
+              xmlns="http://www.w3.org/2000/svg"
+              height="25"
+              width="25"
             >
-              <svg
-                aria-hidden="true"
-                className="svg"
-                viewBox="0 0 30 30"
-                xmlns="http://www.w3.org/2000/svg"
-                height="25"
-                width="25"
-              >
-                <path
-                  d="M20.756 18.876l6.155 6.154-1.88 1.881-6.155-6.155A9.269 9.269 0 0 1 13.3 22.61a9.31 9.31 0 1 1 9.31-9.31c0 2.091-.69 4.021-1.854 5.576zM13.3 19.95a6.65 6.65 0 1 0 0-13.3 6.65 6.65 0 0 0 0 13.3z"
-                  fill="#000000"
-                ></path>
-              </svg>
-            </button>
-         
+              <path
+                d="M20.756 18.876l6.155 6.154-1.88 1.881-6.155-6.155A9.269 9.269 0 0 1 13.3 22.61a9.31 9.31 0 1 1 9.31-9.31c0 2.091-.69 4.021-1.854 5.576zM13.3 19.95a6.65 6.65 0 1 0 0-13.3 6.65 6.65 0 0 0 0 13.3z"
+                fill="#000000"
+              ></path>
+            </svg>
+          </button>
         </div>
-<h1 className="main-title d-flex text-capitalize pt-4">  {city}  Homes for Sale | Real Estate Updated Daily Listings</h1>
-<p>Refine your <span className='text-capitalize'>{city}</span> real estate search by price, bedroom, or type (house, townhouse, or condo). View up-to-date MLS® listings in <span className='text-capitalize'>{city}</span> .</p>
+        <h1 className="main-title d-flex text-capitalize pt-4">
+          {" "}
+          {city} Homes for Sale | Real Estate Updated Daily Listings
+        </h1>
+        <p>
+          Refine your <span className="text-capitalize">{city}</span> real
+          estate search by price, bedroom, or type (house, townhouse, or condo).
+          View up-to-date MLS® listings in{" "}
+          <span className="text-capitalize">{city}</span> .
+        </p>
 
+        <div className="pt-3 row row-cols-md-4 ">
+          {main_data.map((curElem) => {
+            return (
+              <ResoCard
+                city={curElem?.Municipality.toLowerCase()}
+                key={curElem?.MLS}
+                curElem={curElem}
+              />
+            );
+          })}
+        </div>
+      </div>
 
-<div className="pt-3 row row-cols-md-4 ">
-        
-         {data.map((curElem) => {
-              return <ResoCard city={curElem.City.toLowerCase()} key={curElem.ListingId} curElem={curElem} />;
-            })}
-         
-          </div>
-   
-        
-    </div>
-
-    <div>
-     
-    </div>
-    
-
-    
-   
+      <div></div>
     </>
-  )
-}
+  );
+};
 
 export default page;
--- components/reso/ResoCard.js
+++ components/reso/ResoCard.js
@@ -13,43 +13,40 @@ const ResoCard = ({ curElem, city }) => {
     <>
       <div className="col">
         <Link
-          href={`/ontario/${city}/${curElem.ListingId}`}
+          href={`/ontario/${city}/${curElem.MLS}`}
           className="text-decoration-none text-dark"
         >
           <div className="afte-proj">
-          <div className="img-text ">
-              <p className="m-0 ">  {" "}{timeAgo(new Date(curElem.ModificationTimestamp))}</p>
+            <div className="img-text ">
+              <p className="m-0 "> {timeAgo(new Date(curElem.PixUpdtedDt))}</p>
             </div>
             {curElem.Media.length > 0 ? (
               <img src={curElem.Media[0].MediaURL} className="imghei" />
-              
             ) : (
               <p>NO Image</p>
             )}
-          
+
             <div className="card-textt card">
               <p className="mb-0 card-price">{price}</p>
               <p className="mb-0 tet-s d-flex">
                 <span className="">
-                  <i class="fa-solid fa-bed card-icon"></i>{" "}
-                  {curElem.ITSO_BedsAboveGrade + curElem.ITSO_BedsBelowGrade}{" "}
+                  <i class="fa-solid fa-bed card-icon"></i> {curElem.Bedrooms}{" "}
                   Bed
                 </span>
                 <span className="px-4">
-                  <i class="fa-solid fa-bath card-icon"></i>{" "}
-                  {curElem.BathroomsTotalInteger} Bath
+                  <i class="fa-solid fa-bath card-icon"></i> 2 Bath
                 </span>
                 <span className="">
                   <i class="fa-sharp fa-solid fa-ruler"></i>{" "}
-                  {curElem.BuildingAreaTotal} Sqft
+                  {curElem.ApproxSquareFootage} Sqft
                 </span>
               </p>
-              <p className="mb-0 tet-s"> MLS® #{curElem.ListingId}</p>
+              <p className="mb-0 tet-s"> MLS® #{curElem.MLS}</p>
               <p className="mb-0 tet-s">{curElem.UnparsedAddress}</p>
 
               <p className="mb-0 text-small">
                 {" "}
-                Listed by {curElem.ListOfficeName}
+                Listed by {curElem.ListBrokerage}
               </p>
             </div>
           </div>
